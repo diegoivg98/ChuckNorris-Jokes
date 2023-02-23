06375e437a63b027afd2f6a25577a8bf672b8da6
Diego Vistoso <diegoivg98@gmail.com>
2023-02-23 17:18:23 -0300

    agregar botones de Like y Dislike y mostrar contador de cada uno

--- src/components/RandomJokes.jsx
+++ src/components/RandomJokes.jsx
@@ -1,29 +1,49 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Button } from "@mui/material";
-import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import { Button, Box } from "@mui/material";
+import RestartAltIcon from "@mui/icons-material/RestartAlt";
 
 const baseURL = "https://api.chucknorris.io/jokes/random";
 
 const RandomJokes = () => {
-    const [joke, setJoke] = useState(null);
+  const [joke, setJoke] = useState(null);
+  const [like, setLike] = useState(0);
+  const [dislike, setDislike] = useState(0);
 
-    useEffect(() => {
-        getRandomJoke()
-    }, []);
+  useEffect(() => {
+    getRandomJoke();
+  }, []);
 
-    const getRandomJoke = () => {
-        axios.get(baseURL)
-            .then((response) => {
-                setJoke(response.data.value)
-            })
-    }
+  const getRandomJoke = async () => {
+    await axios.get(baseURL).then((response) => {
+      setJoke(response.data.value);
+    });
+  };
 
-    return <div>
-        <h1>Chuck Norris Random Jokes</h1>
-        <p>{joke}</p>
-        <Button onClick={getRandomJoke} variant="contained" endIcon={<RestartAltIcon />}>Generate Random Joke</Button>
-    </div>;
+  const voteLike = () => {
+    setLike(like + 1);
+  };
+
+  const voteDisLike = () => {
+    setDislike(dislike + 1);
+  };
+
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <h1>{joke}</h1>
+    <Button variant="contained" onClick={getRandomJoke}>
+      New Joke
+    </Button>
+    <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 2 }}>
+      <Button variant="contained" onClick={voteLike} sx={{ marginRight: 2 }}>
+        Like ({like})
+      </Button>
+      <Button variant="contained" onClick={voteDisLike}>
+        Dislike ({dislike})
+      </Button>
+    </Box>
+  </Box>
+  );
 };
 
 export default RandomJokes;
