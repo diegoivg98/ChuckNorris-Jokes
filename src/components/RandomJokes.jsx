0945250e7c91a5b49bf85a1595877b02e559bfdf
Diego Vistoso <diegoivg98@gmail.com>
2023-02-23 17:44:39 -0300

    agregar estado de habilitado/deshabilitado a los botones de Like y Dislike al momento de votar

--- src/components/RandomJokes.jsx
+++ src/components/RandomJokes.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Button, Box } from "@mui/material";
-import RestartAltIcon from "@mui/icons-material/RestartAlt";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ThumbDownIcon from "@mui/icons-material/ThumbDown";
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
 
 const baseURL = "https://api.chucknorris.io/jokes/random";
 
@@ -9,40 +11,68 @@ const RandomJokes = () => {
   const [joke, setJoke] = useState(null);
   const [like, setLike] = useState(0);
   const [dislike, setDislike] = useState(0);
+  const [isDisabled, setIsDisabled] = useState(false);
 
   useEffect(() => {
     getRandomJoke();
   }, []);
 
   const getRandomJoke = async () => {
-    await axios.get(baseURL).then((response) => {
-      setJoke(response.data.value);
-    });
+    setIsDisabled(true);
+    const response = await axios.get(baseURL);
+    setJoke(response.data.value);
+    setIsDisabled(false);
   };
 
   const voteLike = () => {
     setLike(like + 1);
+    setIsDisabled(true);
   };
 
   const voteDisLike = () => {
     setDislike(dislike + 1);
+    setIsDisabled(true);
   };
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-    <h1>{joke}</h1>
-    <Button variant="contained" onClick={getRandomJoke}>
-      New Joke
-    </Button>
-    <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 2 }}>
-      <Button variant="contained" onClick={voteLike} sx={{ marginRight: 2 }}>
-        Like ({like})
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        border: "1px solid black",
+        borderRadius: "8px",
+        padding: "16px",
+        backgroundColor: "#f5f5f5",
+        marginBottom: "16px",
+      }}
+    >
+      <h1>{joke}</h1>
+      <Button variant="contained" onClick={getRandomJoke} endIcon={<RestartAltIcon />}>
+        New Joke
       </Button>
-      <Button variant="contained" onClick={voteDisLike}>
-        Dislike ({dislike})
-      </Button>
+      <Box sx={{ display: "flex", justifyContent: "center", marginTop: 2 }}>
+        <Button
+          variant="contained"
+          onClick={voteLike}
+          sx={{ marginRight: 2 }}
+          color="success"
+          disabled={isDisabled}
+          endIcon={<ThumbUpIcon />}
+        >
+          Like ({like})
+        </Button>
+        <Button
+          variant="contained"
+          onClick={voteDisLike}
+          color="error"
+          disabled={isDisabled}
+          endIcon={<ThumbDownIcon />}
+        >
+          Dislike ({dislike})
+        </Button>
+      </Box>
     </Box>
-  </Box>
   );
 };
 
